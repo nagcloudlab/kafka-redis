9fa851a2444d53d9e323dd66b589111da0f1622d
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-15 18:25:55 +0530

    day-1 commit

--- play-with-kafka/code/kafka-clients/src/main/java/com/example/Producer.java
+++ play-with-kafka/code/kafka-clients/src/main/java/com/example/Producer.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -15,13 +16,15 @@ public class Producer {
 
     private static Logger logger = org.slf4j.LoggerFactory.getLogger(Producer.class);
 
+    private static List<String> transactionTypes = List.of("upi","neft","rtgs");
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         // Set the properties for the KafkaProducer
         Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "4.247.148.242:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "4.247.148.242:9092,4.247.148.242:9093,4.247.148.242:9094");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, "com.example.CustomPartitioner");
 
         // Create a KafkaProducer instance
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
@@ -29,16 +32,8 @@ public class Producer {
         // Send a message to the topic "topic1"
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
             String value = "Hey Kafka!".repeat(100); // 1kb message
-            ProducerRecord<String, String> record = new ProducerRecord<>("topic3", value);
-//            Future<RecordMetadata> recordMetadataFuture= producer.send(record);
-//            RecordMetadata recordMetadata=recordMetadataFuture.get();
-//            logger.info("Received new metadata \nTopic: {}\nKey: {}\nPartition: {}\nOffset: {}\nTimestamp: {}",
-//                    recordMetadata.topic(),
-//                    null,
-//                    recordMetadata.partition(),
-//                    recordMetadata.offset(),
-//                    recordMetadata.timestamp());
-
+            String key = transactionTypes.get(i % 3);
+            ProducerRecord<String, String> record = new ProducerRecord<>("topic2", 0, key, value);
             producer.send(record, (recordMetadata, e) -> {
                 if (e == null) {
                     logger.info("Received new metadata \nTopic: {}\nKey: {}\nPartition: {}\nOffset: {}\nTimestamp: {}",
@@ -52,7 +47,7 @@ public class Producer {
                 }
             });
 
-            TimeUnit.MICROSECONDS.sleep(1);
+            TimeUnit.MILLISECONDS.sleep(1);
         }
 
         // Close the producer
