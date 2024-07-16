31647117e931d7d44b98bc068c85b6b9121250c5
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-16 12:56:34 +0530

    kafka producer config

---
+++ play-with-kafka/code/kafka-clients/src/main/java/com/example/ProducerHeaderInterceptor.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.apache.kafka.clients.producer.ProducerInterceptor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Map;
+
+public class ProducerHeaderInterceptor implements ProducerInterceptor {
+
+    @Override
+    public ProducerRecord onSend(ProducerRecord producerRecord) {
+        producerRecord.headers().add("origin", "npci-chennai".getBytes());
+        return producerRecord;
+    }
+
+    @Override
+    public void onAcknowledgement(RecordMetadata recordMetadata, Exception e) {
+    //..
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
